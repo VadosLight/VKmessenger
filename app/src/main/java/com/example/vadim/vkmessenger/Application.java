94649ab2b9d7b5349249987118a40ed2ab6241f4
VadosLight <vadimka-9797@mail.ru>
2018-11-09 23:20:01 +0300

    В главном активити мы узнаём Отпечаток Сертификата

---
+++ app/src/main/java/com/example/vadim/vkmessenger/Application.java
@@ -0,0 +1,13 @@
+package com.example.vadim.vkmessenger;
+
+import com.vk.sdk.VKSdk;
+
+public class Application extends android.app.Application {
+
+    @Override
+    public void onCreate(){
+        super.onCreate();
+
+        VKSdk.initialize(this);
+    }
+}
--- app/src/main/java/com/example/vadim/vkmessenger/MainActivity.kt
+++ app/src/main/java/com/example/vadim/vkmessenger/MainActivity.kt
@@ -3,19 +3,28 @@ package com.example.vadim.vkmessenger
 import android.support.v7.app.AppCompatActivity
 import android.os.Bundle
 import android.widget.Toast
+import com.vk.sdk.VKScope
+import com.vk.sdk.VKSdk
 import com.vk.sdk.util.VKUtil
 import java.util.*
 
 
 class MainActivity : AppCompatActivity() {
 
+
+
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
 
-        val fingerprints = VKUtil.getCertificateFingerprint(this, this.packageName)
-        //String[] fingerprints = VKUtil.getCertificateFingerprint(this, this.getPackageName());
-        System.out.println(fingerprints[0])
+        VKSdk.login(this,
+            VKScope.MESSAGES,
+            VKScope.FRIENDS,
+            VKScope.NOTIFICATIONS,
+            VKScope.WALL,
+            VKScope.OFFLINE)
+
 
     }
 }

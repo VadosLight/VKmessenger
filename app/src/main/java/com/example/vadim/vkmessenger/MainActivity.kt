61789230cc4a49fc3c814651e4e22e3b0add6671
VadosLight <vadimka-9797@mail.ru>
2018-11-09 22:06:16 +0300

    В главном активити мы узнаём Отпечаток Сертификата

--- app/src/main/java/com/example/vadim/vkmessenger/MainActivity.kt
+++ app/src/main/java/com/example/vadim/vkmessenger/MainActivity.kt
@@ -2,11 +2,20 @@ package com.example.vadim.vkmessenger
 
 import android.support.v7.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.Toast
+import com.vk.sdk.util.VKUtil
+import java.util.*
+
 
 class MainActivity : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        val fingerprints = VKUtil.getCertificateFingerprint(this, this.packageName)
+        //String[] fingerprints = VKUtil.getCertificateFingerprint(this, this.getPackageName());
+        System.out.println(fingerprints[0])
+
     }
 }

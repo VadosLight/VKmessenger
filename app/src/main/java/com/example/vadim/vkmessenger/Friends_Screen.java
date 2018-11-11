b4ec80ad12613adb482d28465c72684eeeb65c5e
VadosLight <vadimka-9797@mail.ru>
2018-11-11 04:19:43 +0300

    Тутачки работает ввод голосом

--- app/src/main/java/com/example/vadim/vkmessenger/Friends_Screen.java
+++ app/src/main/java/com/example/vadim/vkmessenger/Friends_Screen.java
@@ -1,39 +1,82 @@
 package com.example.vadim.vkmessenger;
 
 import android.app.Activity;
+import android.content.Intent;
 import android.os.Bundle;
-import android.widget.ArrayAdapter;
-import android.widget.ListView;
-import com.vk.sdk.api.*;
-import com.vk.sdk.api.model.VKList;
+import android.speech.RecognizerIntent;
+import android.view.View;
+import android.widget.TextView;
+import android.widget.Toast;
+
+import java.util.ArrayList;
+import java.util.Locale;
 
 public class Friends_Screen extends Activity{
 
-    private  ListView listView;
+    private TextView txtResult;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_friends__screen);
+        txtResult = (TextView) findViewById(R.id.txtView_Name_User);
+        }
 
-        listView = (ListView) findViewById(R.id.listView);
-        final VKRequest request = VKApi.friends().get(VKParameters.from(VKApiConst.FIELDS,
-                "first_name", "last_name"));
-        request.executeSyncWithListener(new VKRequest.VKRequestListener() {
-            @Override
-            public void onComplete(VKResponse response) {
-                super.onComplete(response);
+    public void getSpeechInput(View view){
+        Intent intent = new Intent(RecognizerIntent.ACTION_RECOGNIZE_SPEECH);
+        intent.putExtra(RecognizerIntent.EXTRA_LANGUAGE_MODEL, RecognizerIntent.LANGUAGE_MODEL_FREE_FORM);
+        intent.putExtra(RecognizerIntent.EXTRA_LANGUAGE, Locale.US);//ВЫбираем язык, который будет восприниматься
 
-                VKList list = (VKList) response.parsedModel;
-                ArrayAdapter<String> arrayAdapter =
-                        new ArrayAdapter<String>(Friends_Screen.this,
-                        android.R.layout.simple_expandable_list_item_1, list);
-
-                listView.setAdapter(arrayAdapter);
-            }
-        });
+        if (intent.resolveActivity(getPackageManager())!= null){
+        startActivityForResult(intent, 10);
+        }
+        else {
+            Toast.makeText(this, "Your device don`t support this function", Toast.LENGTH_LONG).show();
+        }
     }
 
+    @Override
+    protected void onActivityResult(int requestCode, int resultCode, Intent data) {
+        super.onActivityResult(requestCode, resultCode, data);
 
+        switch (requestCode) {
+            case 10:
+                if( data!=null){
+                    ArrayList<String> result =  data.getStringArrayListExtra(RecognizerIntent.EXTRA_RESULTS);
+                    txtResult.setText(result.get(0));
+                    String rlllllll = result.get(0);
+
+                }
+                break;
+        }
+    }
 }
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 
--- app/src/main/java/com/example/vadim/vkmessenger/MainActivity.kt
+++ app/src/main/java/com/example/vadim/vkmessenger/MainActivity.kt
@@ -17,6 +17,8 @@ class MainActivity : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
+
+
         requestWindowFeature(Window.FEATURE_NO_TITLE)
         window.setFlags(WindowManager.LayoutParams.FLAG_FULLSCREEN,
             WindowManager.LayoutParams.FLAG_FULLSCREEN)
